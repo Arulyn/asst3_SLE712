5ae5d3cc614bcf334f1a04338802fa4d0baf6e89
markziemann <mark.ziemann@gmail.com>
2019-05-24 12:35:34 +1000

    changed the count number

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for n in {1..100};do
+for n in {1..200};do
 formatdb -p F  -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
 
 msbar -sequence 9.fa -count 325 -point 4 -block 0 -codon 0 -outseq mysample_mutated.fa
